df6221492f3686ba20c0ef6a7307c617972d4d38
Jesse <jessecdenier@gmail.com>
2024-03-12 14:30:06 -0500

    Changed image expansion to a clickable modal instead of on hover.

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Get all the images in the section
+var images = document.querySelectorAll(".Section_Content_Images img");
+
+// Add click event listener to each image
+images.forEach(function (image) {
+  image.addEventListener("click", function () {
+    expandImage(image);
+  });
+});
+
+function expandImage(image) {
+  // Create a modal element
+  var modal = document.createElement("div");
+  modal.className = "modal";
+
+  // Create an image element inside the modal
+  var modalImg = document.createElement("img");
+  modalImg.src = image.src;
+  modalImg.alt = image.alt;
+  modal.appendChild(modalImg);
+
+  // Append the modal to the document body
+  document.body.appendChild(modal);
+
+  // When the user clicks anywhere outside of the modal, close it
+  modal.addEventListener("click", function () {
+    modal.remove();
+  });
+}
